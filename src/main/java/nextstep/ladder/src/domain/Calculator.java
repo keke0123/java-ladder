9cdaee28910fc06767b9f19b16fe7d33835a8746
keke0123 <keke0123@naver.com>
2021-04-27 21:53:22 +0900

    (refactor) 좀더 clean 하게 수정

--- src/main/java/nextstep/ladder/src/domain/Calculator.java
+++ src/main/java/nextstep/ladder/src/domain/Calculator.java
@@ -26,11 +26,29 @@ public class Calculator {
   }
 
   private int calculateCol(Floor floor, int col) {
-    if(col - 1 >= 0 && floor.floor().get(col - 1).isBridge()) {
+    if(isBridge(floor, col - 1)) {
       return col - 1;
-    } else if (col < floor.floor().size() && floor.floor().get(col).isBridge()) {
+    } else if(isBridge(floor, col)) {
       return col + 1;
     }
     return col;
   }
+
+  private boolean isBridge(Floor floor, int col) {
+    try {
+      return floor.floor().get(col).isBridge();
+    } catch(ArrayIndexOutOfBoundsException e) {
+      return false;
+    }
+  }
+
+  // private boolean isMove(boolean isBridge, int col, int maxSize) {
+  //   if(col < 0) {
+  //     return false;
+  //   }
+  //   if(col >= maxSize) {
+  //     return false;
+  //   }
+  //   return isBridge;
+  // }
 }
